be5e4afc66ae6346d012cf9fd012fb684e1d9838
MWN <91609578+mwn-san@users.noreply.github.com>
2024-05-15 21:08:25 +0700

    Create resume_connector.sh

---
+++ connect/tools/resume_connector.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+KAFKA_CONNECT_URL="http://<host>:<port>"
+
+connectors=$(curl -s "$KAFKA_CONNECT_URL/connectors")
+
+for connector in $(echo $connectors | jq -r '.[]'); do
+    echo "Pausing connector: $connector"
+    curl -X PUT "$KAFKA_CONNECT_URL/connectors/$connector/resume" -o /dev/null
+done
+
+echo "CONNECTORS HAVE BEEN RESUMED."

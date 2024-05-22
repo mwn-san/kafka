c04ff168c7904582e839ccb50ff152c93c6b7250
MWN <91609578+mwn-san@users.noreply.github.com>
2024-05-23 01:21:12 +0700

    Update function_consumer_lag_time.py

--- confluent-kafka-python/calculate-msg-consumption-time/src/function_consumer_lag_time.py
+++ confluent-kafka-python/calculate-msg-consumption-time/src/function_consumer_lag_time.py
@@ -4,3 +4,4 @@ def calculate_time_to_consume_messages(total_messages, messages_per_interval, in
     hours = int(total_seconds // 3600)
     minutes = int((total_seconds % 3600) / 60)
     return hours, minutes
+

5ea342c9022b8998b914a3af35843efdd1564a93
𝑀𝑜ℎ𝑎𝑚𝑚𝑎𝑑 𝑊𝑖𝑙𝑑𝑎𝑛 𝑁𝑢𝑟𝑦𝑢𝑙𝑑𝑎 <91609578+mwn-san@users.noreply.github.com>
2024-05-26 00:16:20 +0700

    Add files via upload

---
+++ python-client-application-for-kafka/calculate-message-consumption-time/src/function_consumer_lag_time.py
@@ -0,0 +1,6 @@
+def calculate_time_to_consume_messages(total_messages, messages_per_interval, interval_duration):
+    throughput_per_second = messages_per_interval / interval_duration
+    total_seconds = total_messages / throughput_per_second
+    hours = int(total_seconds // 3600)
+    minutes = int((total_seconds % 3600) / 60)
+    return hours, minutes
